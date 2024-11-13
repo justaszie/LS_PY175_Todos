61c0e21bb66fd2b32f3c3433e2eb613c0a410bd0
Justas Zieminykas <justaxsz@gmail.com>
2024-11-13 13:06:17 +0200

    Added basic structure: layout and templates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>This is a todo starter!</h1>"
+    return render_template('lists.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
