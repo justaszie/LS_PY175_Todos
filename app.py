89cda8b980d5c7c98d60818f205a6851b2e3c748
Justas Zieminykas <justaxsz@gmail.com>
2024-11-15 11:11:32 +0200

    Added list editing features

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, g, render_template, redirect, url_for, request, session, flash, abort
 from uuid import uuid4
-from utils import error_for_list_title
+from utils import *
+from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
 app.secret_key = 'secret1'
-
 
 @app.before_request
 def initialize_session():
     if 'lists' not in session:
         session['lists'] = []
+
+@app.context_processor
+def list_utilities_processor():
+    return {'is_list_completed': is_list_completed,
+            }
 
 @app.route("/")
 def index():
@@ -18,7 +23,9 @@ def index():
 @app.route("/lists", methods=['GET', 'POST'])
 def lists():
     if request.method == 'GET':
-        return render_template('lists.html', lists=session['lists'], example=None)
+        return render_template('lists.html',
+                               lists=session['lists'],
+                               count_todos_remaining=count_todos_remaining)
     if request.method == 'POST':
         title = request.form.get('list_title').strip()
         error = error_for_list_title(title, session['lists'])
@@ -39,11 +46,137 @@ def lists():
 def new_list():
     return render_template('new_list.html')
 
+
 @app.route('/lists/<list_id>')
 def list_details(list_id):
-    lst = [lst for lst in session['lists'] if lst['id'] == list_id][0]
-    return str(lst)
+    lst = get_list_by_id(list_id, session['lists'])
+    if lst:
+        return render_template('list.html', lst=lst)
+    else:
+        abort(404, description="List not found.")
+        # raise NotFound(description="List not found.")
 
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error_message=error.description), 404
+
+
+@app.route('/lists/<list_id>/complete_all', methods=['POST'])
+def complete_all_todos(list_id):
+    # 1. Validate list
+    todo_list = get_list_by_id(list_id, session['lists'])
+    if not todo_list:
+        abort(404, description='List not found.')
+
+    for todo in todo_list['todos']:
+        todo['completed'] = True
+
+    session.modified = True
+    flash('All todos marked as completed!', 'success')
+
+    return redirect(url_for('list_details', list_id=list_id))
+
+@app.route('/lists/<list_id>/edit', methods=['GET', 'POST'])
+def edit_list(list_id):
+    todo_list = get_list_by_id(list_id, session['lists'])
+    if not todo_list:
+        abort(404, description='List not found.')
+
+    if request.method == 'GET':
+        return render_template('edit_list.html', lst=todo_list)
+
+    if request.method == 'POST':
+        list_title = request.form.get('list_title').strip()
+        error = error_for_list_title(list_title, session['lists'])
+        if error:
+            flash(error, 'error')
+            return render_template('edit_list.html', lst=todo_list)
+
+        todo_list['title'] = list_title
+        session.modified = True
+        flash('List title changed successfully', 'success')
+
+        return redirect(url_for('list_details', list_id=list_id))
+
+@app.route('/lists/<list_id>/delete', methods=['POST'])
+def delete_list(list_id):
+    todo_list = get_list_by_id(list_id, session['lists'])
+    if not todo_list:
+        abort(404, description="List not found.")
+
+    session['lists'].remove(todo_list)
+    session.modified = True
+    flash(f'List "{todo_list['title']}" deleted successfully.', 'success')
+
+    return redirect(url_for('lists'))
+
+@app.route('/lists/<list_id>/todos/<todo_id>/toggle', methods=['POST'])
+def toggle_todo(list_id, todo_id):
+     # 1. List validation
+    todo_list = get_list_by_id(list_id, session['lists'])
+    if not todo_list:
+        abort(404, description="List not found.")
+
+    # 2. Todo validation
+    todo = get_todo_by_id(todo_id, todo_list['todos'])
+    if not todo:
+        abort(404, description="Todo not found.")
+
+
+    # 3. Success - changing todo completion state
+    todo['completed'] = (request.form['completed'] == 'True')
+    session.modified = True
+    flash('Todo state changed successfully.', 'success')
+
+    return redirect(url_for('list_details', list_id=list_id))
+
+
+@app.route('/lists/<list_id>/todos/<todo_id>/delete', methods=['POST'])
+def delete_todo(list_id, todo_id):
+    # 1. List validation
+    todo_list = get_list_by_id(list_id, session['lists'])
+    if not todo_list:
+        abort(404, description="List not found.")
+
+    # 2. Todo validation
+    todo = get_todo_by_id(todo_id, todo_list['todos'])
+    if not todo:
+        abort(404, description="Todo not found.")
+
+
+    # 3. Success - deleting todo
+    todo_list['todos'].remove(todo)
+    session.modified = True
+    flash('Todo deleted.', 'success')
+
+    return redirect(url_for('list_details', list_id=list_id))
+
+# @app.post is same as @app.route(path, methods=['POST'])
+@app.post('/lists/<list_id>/todos')
+def add_todo(list_id):
+
+    # 1. List validation
+    todo_list = get_list_by_id(list_id, session['lists'])
+    if not todo_list:
+        abort(404, description="List not found.")
+    # 2. Todo validation
+    todo_title = request.form.get('todo').strip()
+    error = error_for_todo_title(todo_title)
+    if error:
+        flash(error, 'error')
+        return render_template('list.html', lst=todo_list)
+    # 3. Todo creation
+    todo = {
+        'id': str(uuid4()),
+        'title': todo_title,
+        'completed' : False
+    }
+    todo_list['todos'].append(todo)
+    session.modified = True
+    flash('Todo created successfully!', 'success')
+
+    return redirect(url_for('list_details', list_id=list_id))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
--- utils.py
+++ utils.py
@@ -1,7 +1,27 @@
 def error_for_list_title(list_title, lists):
-    if any(list_title.lower() == lst['title'].lower() for lst in lists):
-        return f'Error: {list_title} already exists.'
-    elif 1 > len(list_title) or len(list_title) > 100:
+    if 1 > len(list_title) or len(list_title) > 100:
         return 'Error: the list title should be between 1 and 100 characters'
+    elif any(list_title.lower() == lst['title'].lower() for lst in lists):
+        return f'Error: {list_title} already exists.'
     else:
         return None
+
+def error_for_todo_title(todo_title):
+    if len(todo_title) < 1:
+        return 'Please enter the title'
+    elif len(todo_title) > 100:
+        return 'Title cannot exceed 100 characters'
+    else:
+        return None
+
+def get_list_by_id(list_id, lists):
+    return next((lst for lst in lists if lst['id'] == list_id), None)
+
+def get_todo_by_id(todo_id, todos):
+    return next((todo for todo in todos if todo['id'] == todo_id), None)
+
+def count_todos_remaining(lst):
+    return sum(1 for todo in lst['todos'] if not todo['completed'])
+
+def is_list_completed(lst):
+    return len(lst['todos']) >= 1 and count_todos_remaining(lst) == 0
